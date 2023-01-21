0e48de1731083e364cbb4f985c00eeed3babf849
bogdan <bgdnv2211@gmail.com>
2023-01-21 20:41:25 +0600

    feat: add requirements

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,19 @@
+import os
+import django
 from django.test import TestCase
 
-# Create your tests here.
+from faker import Faker
+
+fake_gen = Faker('ru_RU')
+
+try:
+    os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
+except ModuleNotFoundError:
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    os.environ['DJANGO_SETTINGS_MODULE'] = \
+        f"{CURRENT_DIR.split('/')[-1]}.settings"
+django.setup()
+
+
+
+
